7e1c63564eed244c370187bd3c57d9880cb8d74a
ranjithdivakaran <renjithdivakaran10@gmail.com>
2023-07-27 16:37:17 +0530

    commit

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   //runapp -->attach our widget tree to the UI
-  runApp(MaterialApp(
-    home: SplashPage(),
+  runApp(MaterialApp(    //default theme of our app
+    home: SplashPage(),  //initial page to be launched
   ));
 }
-
+//stateless means this page does not undergo any statechanges
 class SplashPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {      //Build context locate widgets on the widget tree
     return Scaffold(
       appBar: AppBar(title: Text("my splash page")),
     );
